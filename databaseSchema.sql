3a5123d9e836b5254a5b6cc1253ffdb0977df4cb
Benjamin Sweeney <bends11@vt.edu>
2016-04-12 11:37:48 -0400

    First commit

---
+++ databaseSchema.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS Photo, Favorited, Bought, Location, User;
+
+CREATE TABLE User
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(32) NOT NULL,
+	last_name VARCHAR(32) NOT NULL,
+	email VARCHAR(128) NOT NULL,
+	username VARCHAR(32) NOT NULL,
+	password VARCHAR(32) NOT NULL,
+	cc_number BIGINT,
+	cc_ex_month SMALLINT,
+	cc_ex_year SMALLINT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Favorited
+(
+	user_id INT NOT NULL,
+	movie_id VARCHAR(64) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES User(id)
+	ON DELETE CASCADE,
+	PRIMARY KEY (user_id, movie_id)
+);
+
+CREATE TABLE Bought
+(
+	user_id INT NOT NULL,
+	movie_id VARCHAR(64) NOT NULL,
+	num_tickets INT NOT NULL,
+	cost FLOAT,
+	FOREIGN KEY (user_id) REFERENCES User(id)
+	ON DELETE CASCADE,
+	PRIMARY KEY(user_id, movie_id)
+);
+
+CREATE TABLE Location
+(
+	user_id INT NOT NULL,
+	zipcode INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES User(id)
+	ON DELETE CASCADE,
+	PRIMARY KEY(user_id, zipcode)
+);
+
+CREATE TABLE Photo
+(
+       id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+       extension ENUM('jpeg', 'jpg', 'png') NOT NULL,
+       user_id INT,
+       FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
+);
